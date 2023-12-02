ee81a9fc3054acb54023b7b16c2ce01adcb6ead8
Adrian Zhao <acceleratorsnow@gmail.com>
2023-12-01 23:43:25 -0500

    no logn ours

--- cache/our_cache.py
+++ cache/our_cache.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
-
+from typing import Dict, Tuple, List, Deque
 from cache import BaseCache, CacheRequest
 from collections import deque
 import bisect
+
 
 class SortedList:
     def __init__(self):
@@ -13,8 +14,9 @@ class SortedList:
 
 
 class OurCache(BaseCache):
-    cache_dict: dict
-    cache_evict_queue: deque
+    cache_dict: Dict[Tuple[int, int], int]
+    cache_evict_dict: Dict[int, Deque[Tuple[int, int]]]
+    # cache_evict_queue: deque
     recent_evict_dict: dict
     recent_evict_queue: deque
     evict_blocks: int
@@ -26,7 +28,9 @@ class OurCache(BaseCache):
         self.evict_blocks = self.num_blocks // 10
         self.cache_blocks = self.num_blocks - self.evict_blocks
         self.cache_dict = {}
-        self.cache_evict_queue = deque(maxlen=self.cache_blocks)
+        self.cache_evict_dict = {}
+        for i in range(0, 256):
+            self.cache_evict_dict[i] = deque(maxlen=self.evict_blocks)
         self.recent_evict_dict = {}
         self.recent_evict_queue = deque(maxlen=self.evict_blocks)
 
@@ -47,23 +51,23 @@ class OurCache(BaseCache):
             if len(self.cache_dict) == self.cache_blocks:
                 self.our_evict()
 
-            assert len(self.cache_dict) < self.num_blocks
+            assert len(self.cache_dict) < self.cache_blocks
             self.cache_dict[cache_req.tag] = evict_weight
-
-            evict_queue_entry = cache_req.tag[0], cache_req.tag[1], self.cache_dict[cache_req.tag]
-            bisect.insort(self.cache_evict_queue, evict_queue_entry, key=lambda x: x[2])
+            assert len(self.cache_evict_dict[evict_weight]) < self.evict_blocks
+            self.cache_evict_dict[evict_weight].append(cache_req.tag)
 
     def our_evict(self) -> None:
         self.evicts += 1
-        target = self.cache_evict_queue.popleft()
-        tag = target[0], target[1]
-        assert self.cache_dict.pop(tag) == target[2]
-        assert tag not in self.recent_evict_dict
+        target_weight = 0
+        while target_weight <= 255 and len(self.cache_evict_dict[target_weight]) == 0:
+            target_weight += 1
+        assert target_weight <= 255
+        target_tag = self.cache_evict_dict[target_weight].popleft()
+        assert self.cache_dict.pop(target_tag) == target_weight
+        assert target_tag not in self.recent_evict_dict
 
         if len(self.recent_evict_dict) == self.evict_blocks:
             self.recent_evict_dict.pop(self.recent_evict_queue.popleft())
         assert len(self.recent_evict_dict) < self.evict_blocks
-        self.recent_evict_dict[tag] = target[2]
-        self.recent_evict_queue.append(tag)
-
-
+        self.recent_evict_dict[target_tag] = target_weight
+        self.recent_evict_queue.append(target_tag)
