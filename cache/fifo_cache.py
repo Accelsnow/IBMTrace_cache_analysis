75d14522b1c1f5535b909581faf84971ef5416ac
Adrian Zhao <acceleratorsnow@gmail.com>
2023-12-01 20:58:41 -0500

    accelerate fifo

--- cache/fifo_cache.py
+++ cache/fifo_cache.py
@@ -9,19 +9,20 @@ class FIFOCache(BaseCache):
         super().__init__(size, block_size)
         self.name = "FIFOCache"
         self.queue = deque(maxlen=self.num_blocks)
-
+        self.hiting = set()
     def access(self, cache_req: CacheRequest) -> None:
         self.accesses += 1
-        if cache_req.tag in self.queue:
+        if cache_req.tag in self.hiting:
             self.hits += 1
         else:
             self.misses += 1
             assert len(self.queue) <= self.num_blocks
             if len(self.queue) == self.num_blocks:
-                self.evict()
+                self.hitting.remove(self.evict())
                 assert len(self.queue) < self.num_blocks
             self.queue.append(cache_req.tag)
+            self.hiting.add(cache_req.tag)
 
-    def evict(self) -> None:
+    def evict(self):
         self.evicts += 1
-        self.queue.popleft()
+        self.queue.get()
